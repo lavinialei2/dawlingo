65ba046dce5de5c78739f6f1883148f433d5d013
Haleigh Simington <haleighsreese@gmail.com>
2025-05-22 10:42:27 -0700

    Added new playground icon

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -82,7 +82,7 @@ import { ReactComponent as Playground } from './assets/Playground.svg';
           className="bg-dawlingo-light-pink rounded-full p-2 hover:bg-opacity-80 transition-all"
           aria-label="Go to playground"
         >
-          <Playground width={80} height={80} />
+          <Playground width={80} height={80} fill="white" />
         </playground-button>
       </header>
         
